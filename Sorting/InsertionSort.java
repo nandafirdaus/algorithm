7133503f65bee53ec8203557d056032ed987b834
Nanda Firdaus <nandafirdaus@outlook.com>
2017-12-26 17:11:34 -0500

    Fix approach for insertion sort to increase the efficiency of swapping.

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -3,15 +3,19 @@ import java.util.Arrays;
 public class InsertionSort {
     public static void main(String[] args) {
 
-        int[] unsorted = {5, 7, 3, 9, 2, 4, 8, 4, 6, 2, 3, 9, 7, 6 ,4};
+        int[] unsorted = {6, 4, 3, -5, 10, -7, 25, 0, 4, 8, 9, 2, 3, 6, 4, 1, -12, 100, -50};
     
         for(int i = 0; i < unsorted.length; i++) {
+            int temp = unsorted[i];
             for(int j = i; j > 0; j--) {
-                if(unsorted[j] < unsorted[j-1]) {
-                    int temp = unsorted[j];
+                if(temp < unsorted[j-1]) {
                     unsorted[j] = unsorted[j-1];
-                    unsorted[j-1] = temp;
+
+                    if (j == 1) {
+                        unsorted[j-1] = temp;
+                    }
                 } else {
+                    unsorted[j] = temp;
                     break;
                 }
             }
